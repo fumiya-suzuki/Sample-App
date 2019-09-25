e51065a862352a60bc4c4c9da68a56323a3a6792
suzuki-fumiya <fumiya.89.1216@gmail.com>
2019-09-25 14:58:01 +0000

    only admin access user/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :admin_user, only: :new
   
   def show
     @user = User.find(params[:id])
@@ -23,5 +24,9 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
