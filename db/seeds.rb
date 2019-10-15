034c7c8725949902da2e369687ed611ddae70234
suzuki-fumiya <fumiya.89.1216@gmail.com>
2019-10-15 04:36:58 +0000

    100 users create

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,13 @@ User.create!( name: "Guest User",
               password: "password",
               password_confirmation: "password",
               admin: false)
+              
+100.times do |n|
+  name = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
