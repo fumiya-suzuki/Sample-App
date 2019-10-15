b7032b89d6e5516ae41b0e5c3fd8a6e34c63007b
suzuki-fumiya <fumiya.89.1216@gmail.com>
2019-10-15 08:26:32 +0000

    user-index-show追加完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :admin_user, only: :new
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page], :per_page => 20)
+  end
   
   def show
   end
